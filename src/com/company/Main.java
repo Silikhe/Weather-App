71b10fe1acd73f3743b2e999016edd0e25427003
Silikhe <silaswans@gmail.com>
2021-07-31 22:38:20 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code
+        System.out.println(" | 1  2  3  4  5  6  7  8  9");
     }
 }
