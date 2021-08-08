627458fe50dc1cbef0d7b347a7e7ff01dd8058d0
silikhe <silaswan@gmail.com>
2021-08-09 02:39:46 +0300

    updating readme

---
+++ src/main/java/App.java
@@ -0,0 +1,41 @@
+//import java.io.Console;
+import java.*
+public class App {
+    public static <String> void main (String[] args){
+        Console newConsole = System.console();
+//      prompts User for input
+
+        System.out.println("Input String:");
+        String text = newConsole.readLine();
+
+//      prompts User for input
+
+        System.out.println("Encryption Key:");
+        Integer key = Integer.parseInt(newConsole.readLine());
+
+//      creates new encryption object
+        Encrypt newEncryption = new Encrypt(text, key);
+
+//      Checks for number digits in text to be encrypted
+        if(!newEncryption.isString()){
+            System.out.println("Invalid Entry");
+        }
+
+//      checks to see if key is in range of 1 to 25
+        if (!newEncryption.isInRange()){
+            System.out.println("Key Range is between 1 and 25");
+        }
+
+//      Prints Encrypted text
+        String encryptedText = newEncryption.forward();
+        System.out.println(String.format("Encrypted String: %s", encryptedText));
+
+        //        creates new decryption object
+        Decrypt newDecryption = new Decrypt(encryptedText, key);
+
+        //        Prints Decrypted text
+        String deryptedText = newDecryption.backward();
+        System.out.println(String.format("Decrypted String: %s", deryptedText));
+
+    }
+}
